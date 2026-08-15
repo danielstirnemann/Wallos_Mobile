d88de7b5a8f49fa2f51b5d4bd0e6300a2e15a3de
Daniel Stirnemann <stirnemann.daniel@hotmail.com>
2026-08-15 22:53:59 +0200

    AppBar Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'app_theme.dart';
 
-void main() {
-  runApp(WallosApp());
-}
+void main () => runApp(WallosApp());
 
 class WallosApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppTheme.lightTheme,
       home: Scaffold(
-        appBar: AppBar(title: Text("Wallos")),
+        appBar: AppBar(
+            title: Text("Wallos")
+        ),
         body: ListView(children: [Text("Meine Abos"), Text("Netflix")]),
       ),
     );
