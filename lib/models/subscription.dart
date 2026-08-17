c8da195621d69968152f9f98c303507e145cc088
Daniel Stirnemann <stirnemann.daniel@hotmail.com>
2026-08-17 22:29:40 +0200

    Subscription and API Service init

---
+++ lib/models/subscription.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Subscription {
+  final String name;
+  final double price;
+  final IconData icon;
+
+  Subscription({
+    required this.name,
+    required this.price,
+    required this.icon,
+  });
+}
--- lib/subscription_page.dart
+++ lib/subscription_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/subscription.dart';
 
 class SubscriptionPage extends StatefulWidget {
   const SubscriptionPage({super.key});
@@ -8,12 +9,26 @@ class SubscriptionPage extends StatefulWidget {
 }
 
 class _SubscriptionPageState extends State<SubscriptionPage> {
+
+  final List<Subscription> subscriptions = [
+    Subscription(name: 'Netflix', price: 17.99, icon: Icons.movie),
+    Subscription(name: 'Spotify', price: 10.99, icon: Icons.music_note),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Abonnemente')),
-      body: Text('Hallo'),
-
+      body: ListView.builder(
+          itemCount: subscriptions.length,
+          itemBuilder: (context, index){
+            final item = subscriptions[index];
+            return ListTile(
+              title:Text(item.name),
+              trailing: Text('${item.price} CHF'),
+            );
+          },
+      ),
     );
   }
 }
