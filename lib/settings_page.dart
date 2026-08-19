11168ca02c36a04a84167efbc6f21d89c12202d8
Daniel Stirnemann <stirnemann.daniel@hotmail.com>
2026-08-19 18:55:13 +0200

    Settings API URL

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -10,25 +10,34 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   final TextEditingController _tokenController = TextEditingController();
+  final TextEditingController _urlController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _loadToken(); // 2. Beim Start Token laden
+    _loadSettings();
+  }
+  @override
+  void dispose() {
+    _tokenController.dispose();
+    _urlController.dispose();
+    super.dispose();
   }
 
-  // Laden des Tokens
-  Future<void> _loadToken() async {
+  // Laden der Einstellungen
+  Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
       _tokenController.text = prefs.getString('wallos_api_token') ?? '';
+      _urlController.text = prefs.getString('wallos_api_url') ?? '';
     });
   }
 
   // Speichern des Tokens
   Future<void> _saveSettings() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('wallos_api_token', _tokenController.text); // 3. Speichern
+    await prefs.setString('wallos_api_token', _tokenController.text);
+    await prefs.setString('wallos_api_url', _urlController.text);
 
     if (!mounted) return;
     ScaffoldMessenger.of(context).showSnackBar(
@@ -44,6 +53,16 @@ class _SettingsPageState extends State<SettingsPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            TextField(
+              controller: _urlController,
+              decoration: const InputDecoration(
+                labelText: 'Wallos API URL',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.link),
+              ),
+              keyboardType: TextInputType.url,
+            ),
+            const SizedBox(height: 20),
             TextField(
               controller: _tokenController,
               decoration: const InputDecoration(
